e4636669d4dc9453928e63416349e6148ebe49a2
Mei Chen <ohmayoh@gmail.com>
2025-08-24 16:32:12 +0200

    [test] Added Controller test

---
+++ src/test/kotlin/stackit/coding/challenge/NotificationControllerTest.kt
@@ -0,0 +1,26 @@
+package stackit.coding.challenge
+
+import io.quarkus.test.junit.QuarkusTest
+import io.restassured.RestAssured.given
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.jupiter.api.Test
+
+@QuarkusTest
+class NotificationControllerTest {
+
+    @Test
+    fun createNotificationEndpoint() {
+        val body = Notification("Warning", "hello", "world")
+
+        given()
+            .body(body)
+            .contentType("application/json")
+            .`when`().post("/notifications")
+            .then()
+            .statusCode(200)
+            .body("type", `is`("Warning"))
+            .body("name", `is`("hello"))
+            .body("description", `is`("world"))
+    }
+
+}
