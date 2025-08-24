2b79c67e37b04130238b9502da8336c9f85100f4
Mei Chen <ohmayoh@gmail.com>
2025-08-24 17:12:44 +0200

    [feature] Added Service

---
+++ src/main/kotlin/stackit/coding/challenge/NotificationService.kt
@@ -0,0 +1,10 @@
+package stackit.coding.challenge
+
+import jakarta.enterprise.context.ApplicationScoped
+
+@ApplicationScoped
+class NotificationService {
+
+    fun triageNotification(notification: Notification) {
+    }
+}
