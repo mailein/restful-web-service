4fde478cd51f5f763c46a67967733272a313422c
Mei Chen <ohmayoh@gmail.com>
2025-08-24 17:13:30 +0200

    [feature] Adapted controller

--- src/main/kotlin/stackit/coding/challenge/NotificationController.kt
+++ src/main/kotlin/stackit/coding/challenge/NotificationController.kt
@@ -1,16 +1,19 @@
 package stackit.coding.challenge
 
+import jakarta.inject.Inject
 import jakarta.ws.rs.Consumes
 import jakarta.ws.rs.POST
 import jakarta.ws.rs.Path
-import jakarta.ws.rs.Produces
 import jakarta.ws.rs.core.MediaType
 
 @Path("/notifications")
 class NotificationController {
 
+    @Inject
+    lateinit var notificationService: NotificationService
+
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    fun createNotification(notification: Notification) = notification
+    fun createNotification(notification: Notification) =
+        notificationService.triageNotification(notification)
 }
