cfe685186068fc2d812d53abf0c10d5c40fae8d8
Mei Chen <ohmayoh@gmail.com>
2025-08-24 18:01:43 +0200

    [code] Added logger

--- src/main/kotlin/stackit/coding/challenge/NotificationService.kt
+++ src/main/kotlin/stackit/coding/challenge/NotificationService.kt
@@ -1,10 +1,14 @@
 package stackit.coding.challenge
 
 import jakarta.enterprise.context.ApplicationScoped
+import org.jboss.logging.Logger
 
 @ApplicationScoped
 class NotificationService {
 
+    val LOG: Logger = Logger.getLogger(this.javaClass)
+
     fun triageNotification(notification: Notification) {
+        LOG.debug("Triage notification: $notification")
     }
 }
