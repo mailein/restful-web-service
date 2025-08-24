1317dbe27a5e3f35aa9fa489122084da3975a9b8
Mei Chen <ohmayoh@gmail.com>
2025-08-24 19:29:26 +0200

    [bug] JSON keys in the payload should be capitalized

--- src/main/kotlin/stackit/coding/challenge/Notification.kt
+++ src/main/kotlin/stackit/coding/challenge/Notification.kt
@@ -1,8 +1,13 @@
 package stackit.coding.challenge
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class Notification(
+    @JsonProperty("Type")
     val type: String,
+    @JsonProperty("Name")
     val name: String,
+    @JsonProperty("Description")
     val description: String,
 ) {
     override fun toString() = "${name} (${description})"
