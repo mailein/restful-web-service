a5197febc74766e49e695aa1dba4fa5211555228
Mei Chen <ohmayoh@gmail.com>
2025-08-24 18:50:45 +0200

    [test] Tested service

---
+++ src/test/kotlin/stackit/coding/challenge/NotificationServiceTest.kt
@@ -0,0 +1,35 @@
+package stackit.coding.challenge
+
+import io.quarkus.test.InjectMock
+import io.quarkus.test.junit.QuarkusTest
+import jakarta.inject.Inject
+import org.eclipse.microprofile.rest.client.inject.RestClient
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+
+@QuarkusTest
+class NotificationServiceTest {
+    @Inject
+    lateinit var notificationService: NotificationService
+
+    @InjectMock
+    @RestClient
+    lateinit var slack: SlackHttpClient
+
+    @Test
+    fun `a warning should trigger slack msg`() {
+        val notification = Notification("Warning", "hello", "world")
+
+        notificationService.triageNotification(notification)
+        Mockito.verify(slack).sendWarning(notification)
+    }
+
+    @Test
+    fun `an info should NOT trigger slack msg`() {
+        val notification = Notification("Info", "hello", "world")
+
+        notificationService.triageNotification(notification)
+        Mockito.verify(slack, Mockito.never()).sendWarning(notification)
+    }
+
+}
