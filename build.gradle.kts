b274d66f86054ce36dae3d9b5fa7955e6304b644
Mei Chen <ohmayoh@gmail.com>
2025-08-24 18:02:21 +0200

    [lib] Added rest client

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ val quarkusPlatformArtifactId: String by project
 val quarkusPlatformVersion: String by project
 
 dependencies {
+    implementation("io.quarkus:quarkus-rest-client-jackson")
     implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
     implementation("io.quarkus:quarkus-kotlin")
     implementation("io.quarkus:quarkus-rest-jackson")
